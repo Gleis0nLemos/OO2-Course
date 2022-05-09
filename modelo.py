7325db5a185c8befd92371e91a7ccf5e75e52e0a
Gleison <gleison04lemos@gmail.com>
2022-05-09 15:25:53 -0300

    Deixando o código mais enxuto e utilizando o conceito de herança

--- modelo.py
+++ modelo.py
@@ -1,57 +1,47 @@
 #Class são usadas para criação de objetos
-class Filme:
+class Programa:
     #inicializador __init__(self ... valores chamados de 'atributos' atribuidos a instância/objetos
+    def __init__(self, nome, ano):
+        self._nome = nome.title()#title deixa as palavras a serem exibidas com a primeira letra maiúscula
+        self.ano = ano
+        self._likes = 0 #O número de likes só irá mudar de acordo com a interação
+
+                                    #o property permite que você declare uma função para obter o valor
+                                    #de um atributo, e, opcionalmente, funções para funcionarem como
+    @property                       #o 'setter' e 'deleter' daquele atributo. dentro da classe.
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def add_likes(self):
+        self._likes += 1
+
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()#title deixa as palavras a serem exibidas com a primeira letra maiúscula
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0 #O número de likes só irá mudar de acordo com a interação
 
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def add_likes(self):
-        self.__likes += 1
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def add_likes(self):
-        self.__likes += 1
-
-
 
 vingadores = Filme("Vingadores: Guerra Infinita", 2018, 160)
 vingadores.add_likes()
+vingadores.add_likes()
 #f, usado para formatar a apresentação do print
-print(f"Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}")
+print(f"{vingadores.nome} - {vingadores.ano} - {vingadores.duracao}min : {vingadores.likes} Likes")
 
 atlanta = Serie("Atlanta", 2018, 2)
 atlanta.add_likes()
 atlanta.add_likes()
-print(f"Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}")
+atlanta.add_likes()
+print(f"{atlanta.nome} - {atlanta.ano} - {atlanta.temporadas} Temporadas : {atlanta.likes} Likes")
