1cd1ad649fb5a741dd35dc67a063dcf28b9199dc
Gleison <gleison04lemos@gmail.com>
2022-05-09 20:10:27 -0300

    Botando em prática os conceitos de polimorfismo e representação textual de objetos

--- modelo.py
+++ modelo.py
@@ -23,25 +23,43 @@ class Programa:
     def add_likes(self):
         self._likes += 1
 
+    def __str__(self):
+        return f"{self._nome} - {self.ano} : {self._likes} Likes"
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f"{self._nome} - {self.ano} - {self.duracao}min   {self._likes} Likes"
+
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f"{self._nome} - {self.ano} - {self.temporadas} Temporadas   {self._likes} Likes"
+
 vingadores = Filme("Vingadores: Guerra Infinita", 2018, 160)
 vingadores.add_likes()
 vingadores.add_likes()
 #f, usado para formatar a apresentação do print
-print(f"{vingadores.nome} - {vingadores.ano} - {vingadores.duracao}min : {vingadores.likes} Likes")
+# print(f"{vingadores.nome} - {vingadores.duracao}min : {vingadores.likes} Likes")
 
 atlanta = Serie("Atlanta", 2018, 2)
 atlanta.add_likes()
 atlanta.add_likes()
 atlanta.add_likes()
-print(f"{atlanta.nome} - {atlanta.ano} - {atlanta.temporadas} Temporadas : {atlanta.likes} Likes")
+# print(f"{atlanta.nome} - {atlanta.temporadas} Temporadas : {atlanta.likes} Likes")
+
+
+
+filmes_series = [vingadores, atlanta]           #para criar uma playlist, fazer uma lista nos ajudará a
+                                                #ter um acesso mais especificado para buscas de nosso programa de TV
+
+for programa in filmes_series:
+    print(programa)
+    # detalhes = programa.duracao if hasattr(programa, "duracao") else programa.temporadas
+    # print(f"{programa.nome} - {detalhes} : {programa.likes} Likes")
